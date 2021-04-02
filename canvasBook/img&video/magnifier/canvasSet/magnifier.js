12a5008ffe96ca9b2583cc27a86d7696ff4f9192
yuchaocheng <yuchaocheng@hikvision.com.cn>
2021-04-02 09:34:49 +0800

    fit:放大镜核心功能已完成

--- canvasBook/img&video/magnifier/canvasSet/magnifier.js
+++ canvasBook/img&video/magnifier/canvasSet/magnifier.js
@@ -14,31 +14,47 @@ export default class Magnifier {
         }
         this.offsetCanvas = Utils.createOffsetCanvas(this.canvas);
         this.offsetCtx = this.offsetCanvas.getContext("2d");
+        this.bgCanvas = Utils.createOffsetCanvas(this.canvas);
+        this.bgCtx = this.bgCanvas.getContext("2d");
         options = Object.assign({}, DEFAULTOPTIONS, options);
         this.ctx = this.canvas.getContext("2d");
-        this.times = options.times || 1; //放大倍数，默认为1
-        this.LensRadius = options.LensRadius || 100; //放大镜大小，默认100
+        this.times = options.times || 1.5; //放大倍数，默认为1
+        this.LensRadius = options.LensRadius || 120; //放大镜大小，默认100
         this.rectImageData = null; //放大区域矩形imageData保存
         this.init();
+        this.isDragging = false;
+        this.currentBall = { x: -1, y: -1, offsetX: -1, offsetY: -1 }; //小球当前信息
     }
     async init() {
         await this.loadBackground();
-        this.magnify(460, 100);
-        this.drawMagnifierCircle(460, 410);
+        this.magnify(460, 410);
+        this.drawMagnifierCircle(460, 410, true);
+        this.dealMouseDown = this.dealMouseDown.bind(this);
+        this.dealMouseMove = Utils.throttle(this.dealMouseMove, 20).bind(this);
+        this.dealMouseUp = this.dealMouseUp.bind(this);
+        this.canvas.addEventListener("mousedown", this.dealMouseDown);
+        this.canvas.addEventListener("mousemove", this.dealMouseMove);
+        this.canvas.addEventListener("mouseup", this.dealMouseUp);
     }
     // 加载背景
     loadBackground() {
         return new Promise((resolve) => {
             let image = new Image();
-            image.src = "../../../image/camp.png";
+            image.src = "../../image/camp.png";
             image.onload = () => {
                 this.ctx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+                /* 背景数据保存在bgCanvas中，后续要使用多个时候直接调用即可 */
+                this.bgCtx.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
                 resolve();
             };
         });
     }
+    drawBackground() {
+        this.ctx.drawImage(this.bgCanvas, 0, 0);
+    }
     // 绘制放大镜实例
-    drawMagnifierCircle(x, y) {
+    drawMagnifierCircle(x, y, isInit) {
+        // ctx.fillRect()
         const { ctx, LensRadius } = this;
         ctx.lineWidth = MagnifierCircleR;
         ctx.save();
@@ -58,18 +74,66 @@ export default class Magnifier {
         ctx.strokeStyle = gradient;
         ctx.stroke();
         ctx.restore();
+        if (isInit) {
+            /* 记录一下小球位置，下次鼠标移动时计算偏移量 */
+            this.currentBall.x = x;
+            this.currentBall.y = y;
+        }
     }
     // 放大
     magnify(rx, ry) {
-        const { ctx, LensRadius, offsetCanvas,offsetCtx } = this;
-        let x = rx - LensRadius / 2;
-        let y = ry - LensRadius / 2;
-        this.rectImageData = ctx.getImageData(x, y, this.LensRadius, this.LensRadius);
-        offsetCtx.clearRect(0, 0, offsetCanvas.width, offsetCanvas.height);
-        offsetCtx.putImageData()
+        const { ctx, LensRadius, offsetCanvas, offsetCtx, times } = this;
+        let x = rx - LensRadius;
+        let y = ry - LensRadius;
+        let scaleSize = times * 2 * LensRadius;
+        offsetCanvas.width = scaleSize;
+        offsetCanvas.height = scaleSize;
+        offsetCtx.save();
+        /* 关键步骤：平移画布，使离屏画布左上角实际绘制的是放大后图形中心部分。目的是主画布再drawImage时，不用再缩放和切割，提升性能 */
+        offsetCtx.translate(-scaleSize / 2 + LensRadius, -scaleSize / 2 + LensRadius);
+        offsetCtx.drawImage(this.canvas, x, y, this.LensRadius * 2, this.LensRadius * 2, 0, 0, scaleSize, scaleSize);
+        offsetCtx.restore();
+        ctx.save();
+        ctx.arc(rx, ry, LensRadius - MagnifierCircleR / 2, 0, Math.PI * 2);
+        ctx.clip();
+        ctx.drawImage(offsetCanvas, x, y);
+        ctx.restore();
     }
     // 设置放大倍速
     setTimes(times) {
         this.times = times;
+    }
+    draw(currentX, currentY) {
+        const { currentBall } = this;
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawBackground();
+        let cx = currentX - currentBall.offsetX;
+        let cy = currentY - currentBall.offsetY;
+        this.magnify(cx, cy);
+        this.drawMagnifierCircle(cx, cy);
+        return { x: cx, y: cy };
+    }
+    dealMouseDown(e) {
+        const { currentBall } = this;
+        currentBall.offsetX = e.offsetX - currentBall.x;
+        currentBall.offsetY = e.offsetY - currentBall.y;
+        this.isDragging = true;
+    }
+    dealMouseMove(e) {
+        if (this.isDragging) {
+            this.draw(e.offsetX, e.offsetY);
+        }
+    }
+    dealMouseUp(e) {
+        const { currentBall } = this;
+        if (this.isDragging) {
+            let circleInfo = this.draw(e.offsetX, e.offsetY);
+            // 初始化数据
+            currentBall.x = circleInfo.x;
+            currentBall.y = circleInfo.y;
+            currentBall.offsetX = -1;
+            currentBall.offsetY = -1;
+            this.isDragging = false;
+        }
     }
 }
